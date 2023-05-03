002c60b8bc56ad5c8d1fe21cb18e43c0961f449c
huiruo <2196411859@qq.com>
2023-05-03 18:40:53 +0800

    feat: test docker

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,22 +1,59 @@
 import { SourceCodeEditor } from "@editor/SourceCodeEditor";
-import { fetchCode } from "@services/api";
+import { fetchCode, getRunningContainerApi, getContainerStatusApi, runCodeApi } from "@services/api";
 import { Button } from "ui";
 
 export default function Web() {
 
-  const onRun = async () => {
+  // const onRun = async () => {
+  //   console.log('onRun-->');
+  //   const res = await fetchCode()
+  //   console.log('res', res)
+  //   const data = await res.json();
+  //   console.log('res-data', data)
+  // }
+
+  const onRunJs = async () => {
     console.log('onRun-->');
-    const res = await fetchCode()
+    const res = await runCodeApi()
     console.log('res', res)
     const data = await res.json();
-    console.log('res-data', data)
+    console.log('onRunJs', data)
+  }
+
+  const onGetContainerStatus = async () => {
+    console.log('onRun-->');
+    const res = await getContainerStatusApi()
+    const data = await res.json();
+    console.log('getRunningContainerApi', data)
+  }
+
+  const onGetRunningContainer = async () => {
+    console.log('onRun-->');
+    const res = await getRunningContainerApi()
+    const data = await res.json();
+    console.log('getRunningContainerApi', data)
   }
 
   return (
     <div>
       <h1>Web</h1>
-      <Button />
-      <button onClick={onRun}>run</button>
+      {/* <Button /> */}
+      {/* <div>
+        <button onClick={onRun}>run</button>
+      </div> */}
+
+      <div>
+        <button onClick={onRunJs}>run js</button>
+      </div>
+
+      <div>
+        <button onClick={onGetContainerStatus}>get Container Status</button>
+      </div>
+
+      <div>
+        <button onClick={onGetRunningContainer}>get Running Container</button>
+      </div>
+
       <div>
         <SourceCodeEditor />
       </div>
--- apps/web/services/api.ts
+++ apps/web/services/api.ts
@@ -17,3 +17,58 @@ export async function fetchCode(params?: any): Promise<any> {
     }
   }
 }
+
+export async function runCodeApi(params?: any): Promise<any> {
+  try {
+    return fetch(`${baseUrl}/code-engine/codeList`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    })
+  } catch (error) {
+    console.error('NetWork Error', error)
+    return {
+      code: 0,
+      message: error,
+    }
+  }
+}
+
+
+export async function getContainerStatusApi(params?: any): Promise<any> {
+  try {
+    return fetch(`${baseUrl}/code-engine/getContainerStatus`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    })
+  } catch (error) {
+    console.error('NetWork Error', error)
+    return {
+      code: 0,
+      message: error,
+    }
+  }
+}
+
+export async function getRunningContainerApi(params?: any): Promise<any> {
+  try {
+    return fetch(`${baseUrl}/code-engine/getRunningContainer`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    })
+  } catch (error) {
+    console.error('NetWork Error', error)
+    return {
+      code: 0,
+      message: error,
+    }
+  }
+}
