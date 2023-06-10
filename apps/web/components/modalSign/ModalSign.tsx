585c962f15b84d1bc90c840f1b8d97b82634ff9f
ruo <ruomechen@gmail.com>
2023-06-10 15:27:04 +0800

    feat: login config

--- apps/web/components/modalSign/ModalSign.tsx
+++ apps/web/components/modalSign/ModalSign.tsx
@@ -1,15 +1,22 @@
 import NiceModal, { useModal } from '@common/nice-modal'
-import {Modal} from "antd";
-import {useSignStatus} from "@components/modalSign/hooks";
+import { Modal } from "antd";
+import { useSignStatus } from "@components/modalSign/hooks";
 
-export const ModalSign = NiceModal.create(({}) => {
+export const ModalSign = NiceModal.create(({ }) => {
     const { status } = useSignStatus()
     const { visible, hide } = useModal()
+    const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENTiD
+    const HOST = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_DEV_HOST : process.env.NEXT_PUBLIC_PROD_HOST
+    const googleRedirectUri = `${HOST}/user/google/auth/handler`
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=${googleRedirectUri}&scope=profile email&client_id=${googleClientId}`
+
+    console.log('process.env.NODE_ENV:', process.env.NODE_ENV, { HOST, googleAuthUrl })
+
     return (
         <Modal open={visible} onCancel={hide}>
-                <div>
-                    test
-                </div>
+            <div>
+                <a href={googleAuthUrl}>登录</a>
+            </div>
         </Modal>
     )
 })
