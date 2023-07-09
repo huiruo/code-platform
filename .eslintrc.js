ee4b28876dc784a8fd417f112adfeee956f22d69
ruo <ruomechen@gmail.com>
2023-07-10 00:09:14 +0800

    ci: hasky && eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,27 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: ['custom'],
   settings: {
     next: {
-      rootDir: ["apps/*/"],
+      rootDir: ['apps/*/'],
     },
   },
-};
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  /**
+   * https://eslint.org/docs/latest/rules
+   */
+  rules: {
+    'no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+}
--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
   root: true,
-  extends: ["custom"],
-};
+  extends: ['custom'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  /**
+   * https://eslint.org/docs/latest/rules
+   */
+  rules: {
+    'no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+}
